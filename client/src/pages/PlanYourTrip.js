7af79396f9afcc8960c264d61085ea8086a6848f
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-06-01 20:41:03 -0700

    deleted extra card deck

--- client/src/pages/PlanYourTrip.js
+++ client/src/pages/PlanYourTrip.js
@@ -140,11 +140,7 @@ return (
             </Form.Group>
           </Form>
         </div>
-          <CardDeck className="row " >
-            {isLoading ? 'Loading' : guide.map((el, index) => {
-            return <GuideCards  key = {index} images = {el.images[0]  ?  el.images[0].url : "https://www.nps.gov/common/uploads/structured_data/CAD2D1A7-09C6-7F1B-C8A2D91D6699D14D.jpg"} name = {el.name} description = {el.description}  regulation = {el.regulationsOverview} /> }
-            )} 
-          </CardDeck>
+          
         </div> 
         
         <div >
