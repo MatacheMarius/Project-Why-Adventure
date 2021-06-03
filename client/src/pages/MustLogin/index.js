a4e6ae6d79680b5a9c7989fe37ecd3bdff9410a6
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-06-03 00:18:27 -0700

    added carousel to mustlogin page

--- client/src/pages/MustLogin/index.js
+++ client/src/pages/MustLogin/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./style.css"
-import { Row, Col, Card, Button, Container, Jumbotron } from "react-bootstrap";
+import { Row, Col, Carousel, Button, Container, Jumbotron } from "react-bootstrap";
+
 
 function MustLogin() {
     return(
@@ -10,18 +11,44 @@ function MustLogin() {
                     <div id="heading">
                         <h4 className="mx-auto display-4">Begin your adventure now</h4>
                     </div>
-                    <Row className="my-4">
+                    <Row className="mt-4 pb-4">
                         <Col><Button className="bg-dark" id="link-button" href="/login">Go To Login Page</Button></Col>
                         <Col><Button className="bg-dark" id="link-button" href="/signup">Go To Sign Up Page</Button></Col>
-                        {/* className="link-button"
-                        className="link-button" */}
                     </Row>
                 </Container>   
             </Jumbotron>
+            <Carousel id="carousel" fade interval={5000} controls={false} pause={false} indicators={false}>
+                <Carousel.Item className="carousel-item">
+                    <img
+                    className="d-block w-100"
+                    src="/images/img-10.jpg"
+                    alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="carousel-item">
+                    <img
+                    className="d-block w-100"
+                    src="/images/img-12.jpg"
+                    alt="Second slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="carousel-item">
+                    <img
+                    className="d-block w-100"
+                    src="/images/img-11.jpg"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item className="carousel-item">
+                    <img
+                    className="d-block w-100"
+                    src="/images/img-13.jpg"
+                    alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>  
         </div>
     )
 }
 
 export default MustLogin
-
-
