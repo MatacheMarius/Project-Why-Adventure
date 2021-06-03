6cc22c9752ab9f90032a851195e6b61eb719b308
Shayla Stevenson <74476502+Shazzyshay@users.noreply.github.com>
2021-06-03 11:19:57 -0700

    added MustLogin route to Trip and Planyourtrip page

--- client/src/pages/PlanYourTrip.js
+++ client/src/pages/PlanYourTrip.js
@@ -6,6 +6,8 @@ import EmployeeModal from '../components/GuideModal';
 import { getUsers } from '../utils/API';
 import {  Form } from "react-bootstrap";
 import axios from "axios";
+import MustLogin from "../pages/MustLogin";
+import { useLoginContext } from "../utils/GlobalState";
 
 
 export default function Map() {
@@ -17,14 +19,14 @@ export default function Map() {
   const [randomGuides, setAllRandomGuides] = useState(null);
   const [isLoading2, setIsLoading2] = useState(true);
   const [nextGuide, getNextGuide] = useState(false);
+  // to manage the users logged in status
+  const [state, dispatch] = useLoginContext();
   const handleClose = () => setIsOpen(false);
   const showModal = () => {setIsOpen(true); } 
   const gotNextGuide = () => getNextGuide(!nextGuide)
 
 
   const getInfo = () => {
-
-
     var options = {
       method: 'GET',
       url: 'https://jonahtaylor-national-park-service-v1.p.rapidapi.com/campgrounds',
@@ -36,130 +38,113 @@ export default function Map() {
       }
     }
     return axios.request(options)
-    
   }
 
+  useEffect(() => {
+  getInfo()
+  .then(response => {
+    if(response.data){
+    setAllGuides(response.data.data)
+    setIsLoading(false);}
+  })
+  .catch(error=> console.log(error));
+  }, [input])
 
-   
-    useEffect(() => {
-    getInfo()
+  useEffect(() => {
+    getUsers()
     .then(response => {
       if(response.data){
-      setAllGuides(response.data.data)
-      setIsLoading(false);}
-      
+      setAllRandomGuides(response.data.results)
+      setIsLoading2(false);
+    }  
     })
     .catch(error=> console.log(error));
-    
-    }, [input])
+  },  [isOpen, nextGuide])
+  console.log(guide);
 
-      
-    useEffect(() => {
-      getUsers()
-      .then(response => {
-        if(response.data){
-        setAllRandomGuides(response.data.results)
-        setIsLoading2(false);
-     
-      }
-        
-      })
-      .catch(error=> console.log(error));
-      
-      },  [isOpen, nextGuide])
-
-
-      console.log(guide);
-
-return (
-    <div>
-          <div>
-          <br/>
+  if(state.user.user_id) {
+    return(
+      <div>
+        <div>
+        <br/>
           <h3 className='selectTitle'>Select A State</h3>
           <div className="stateStyle">
-          <Form value = {input} onChange = {(event) => setInput(event.target.value)} onClick={console.log( input )}>
-              
-            <Form.Group controlId="exampleForm.ControlSelect1">
-              <Form.Label>Search for the State</Form.Label>
-              <Form.Control as="select">
-                
-                <option></option>         
-                <option value = "AL">AL</option>
-                <option value = "AK">AK</option>
-                <option value = "AZ">AZ</option>
-                <option value = "AR">AR</option>
-                <option value = "CA">CA</option>
-                <option value = "CO">CO</option>
-                <option value = "CT">CT</option>
-                <option value = "DE">DE</option>
-                <option value = "FL">FL</option>
-                <option value = "GA">GA</option>
-                <option value = "HI">HI</option>
-                <option value = "ID">ID</option>
-                <option value = "IL">IL</option>
-                <option value = "IN">IN</option>
-                <option value = "IA">IA</option>
-                <option value = "KS">KS</option>
-                <option value = "KY">KY</option>
-                <option value = "LA">LA</option>
-                <option value = "ME">ME</option>
-                <option value = "MD">MD</option>
-                <option value = "MA">MA</option>
-                <option value = "MI">MI</option>
-                <option value = "MN">MN</option>
-                <option value = "MS">MS</option>
-                <option value = "MO">MO</option>
-                <option value = "MT">MT</option>
-                <option value = "NE">NE</option>
-                <option value = "NV">NV</option>
-                <option value = "NH">NH</option>
-                <option value = "NH">NJ</option>
-                <option value = "NM">NM</option>
-                <option value = "NY">NY</option>
-                <option value = "NC">NC</option>
-                <option value = "ND">ND</option>
-                <option value = "OH">OH</option>
-                <option value = "OK">OK</option>
-                <option value = "OR">OR</option>
-                <option value = "PA">PA</option>
-                <option value = "RI">RI</option>
-                <option value = "SC">SC</option>
-                <option value = "SD">SD</option>
-                <option value = "TN">TN</option>
-                <option value = "TX">TX</option>
-                <option value = "UT">UT</option>
-                <option value = "VT">VT</option>
-                <option value = "VA">VA</option>
-                <option value = "WA">WA</option>
-                <option value = "WV">WV</option>
-                <option value = "WI">WI</option>
-                <option value = "WY">WY</option>
-
-              </Form.Control>
-            </Form.Group>
-          </Form>
-        </div>
-          
+            <Form value = {input} onChange = {(event) => setInput(event.target.value)} onClick={console.log( input )}> 
+              <Form.Group controlId="exampleForm.ControlSelect1">
+                <Form.Label>Search for the State</Form.Label>
+                <Form.Control as="select">  
+                  <option></option>         
+                  <option value = "AL">AL</option>
+                  <option value = "AK">AK</option>
+                  <option value = "AZ">AZ</option>
+                  <option value = "AR">AR</option>
+                  <option value = "CA">CA</option>
+                  <option value = "CO">CO</option>
+                  <option value = "CT">CT</option>
+                  <option value = "DE">DE</option>
+                  <option value = "FL">FL</option>
+                  <option value = "GA">GA</option>
+                  <option value = "HI">HI</option>
+                  <option value = "ID">ID</option>
+                  <option value = "IL">IL</option>
+                  <option value = "IN">IN</option>
+                  <option value = "IA">IA</option>
+                  <option value = "KS">KS</option>
+                  <option value = "KY">KY</option>
+                  <option value = "LA">LA</option>
+                  <option value = "ME">ME</option>
+                  <option value = "MD">MD</option>
+                  <option value = "MA">MA</option>
+                  <option value = "MI">MI</option>
+                  <option value = "MN">MN</option>
+                  <option value = "MS">MS</option>
+                  <option value = "MO">MO</option>
+                  <option value = "MT">MT</option>
+                  <option value = "NE">NE</option>
+                  <option value = "NV">NV</option>
+                  <option value = "NH">NH</option>
+                  <option value = "NH">NJ</option>
+                  <option value = "NM">NM</option>
+                  <option value = "NY">NY</option>
+                  <option value = "NC">NC</option>
+                  <option value = "ND">ND</option>
+                  <option value = "OH">OH</option>
+                  <option value = "OK">OK</option>
+                  <option value = "OR">OR</option>
+                  <option value = "PA">PA</option>
+                  <option value = "RI">RI</option>
+                  <option value = "SC">SC</option>
+                  <option value = "SD">SD</option>
+                  <option value = "TN">TN</option>
+                  <option value = "TX">TX</option>
+                  <option value = "UT">UT</option>
+                  <option value = "VT">VT</option>
+                  <option value = "VA">VA</option>
+                  <option value = "WA">WA</option>
+                  <option value = "WV">WV</option>
+                  <option value = "WI">WI</option>
+                  <option value = "WY">WY</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </div>  
         </div> 
-        
-        <div >
+        <div>
           <CardDeck className="row " >
           {isLoading ? 'Loading' : 
-          guide.map((el, index) => {
-          return <GuideCards  key = {index} images = {el.images[0]  ?  el.images[0].url : "https://www.nps.gov/common/uploads/structured_data/CAD2D1A7-09C6-7F1B-C8A2D91D6699D14D.jpg"} name = {el.name} description = {el.description}  regulation = {el.regulationsOverview} showModal = {showModal}  /> }
-          ) } 
+            guide.map((el, index) => {
+              return <GuideCards  key = {index} images = {el.images[0]  ?  el.images[0].url : "https://www.nps.gov/common/uploads/structured_data/CAD2D1A7-09C6-7F1B-C8A2D91D6699D14D.jpg"} name = {el.name} description = {el.description}  regulation = {el.regulationsOverview} showModal = {showModal}  /> }
+          )} 
           </CardDeck>
           {isLoading2 ? 'Loading' : 
-          randomGuides.map((el = 2, index) => {
-            return < EmployeeModal key ={index} isOpen = {isOpen}   image = {el.picture.large}  namef = {el.name.first} namel = {el.name.last} gender = {el.gender} age={el.dob.age} experience = {el.registered.age} cell={el.cell}  email={el.email} handleClose = {handleClose} next = {gotNextGuide} />  })
-           
-            }
+            randomGuides.map((el = 2, index) => {
+              return < EmployeeModal key ={index} isOpen = {isOpen}   image = {el.picture.large}  namef = {el.name.first} namel = {el.name.last} gender = {el.gender} age={el.dob.age} experience = {el.registered.age} cell={el.cell}  email={el.email} handleClose = {handleClose} next = {gotNextGuide} />  }
+          )}
         </div>
-        </div>
-        
-        )
-  
-    
-
+      </div>
+  )} else {
+    return (
+      <MustLogin />
+    )
+  }
 };
-
--- client/src/pages/Trips.js
+++ client/src/pages/Trips.js
@@ -3,11 +3,10 @@ import DeleteBtn from "../components/DeleteBtn";
 import { BookedAdventureCard, BookedDeck } from "../components/BookedAdventureCard"
 import MustLogin from "../pages/MustLogin"
 import tripsAPI from "../utils/tripsAPI";
-import { Row, Col, Card, ListGroupItem } from "react-bootstrap";
+import { Row, Col, Card, ListGroupItem, Carousel } from "react-bootstrap";
 import { useLoginContext } from "../utils/GlobalState";
 import guidesAPI from "../utils/guidesAPI";
  
-
 function Trips() {
   const [trips, setTrips] = useState([])
   const [guides, setGuides] = useState([])
@@ -46,17 +45,11 @@ function Trips() {
       
   };
 
-
-
-
-
   function deleteTrip(id) {
     tripsAPI.deleteTrip(id)
     .then(res => loadTrips())
     .catch(err => console.log(err));
   }
-
-
 
   function deleteGuide(id) {
     guidesAPI.deleteGuide(id)
@@ -64,112 +57,80 @@ function Trips() {
     .catch(err => console.log(err));
   }
 
+  const y = trips.filter(booked => booked.booked === true)
+  console.log("guides = ", guides)
+  console.log("y = ", y)
+  let myArr = [];
+  for (let i = 0; i < guides.length; i ++) {
+  myArr.push(guides[i]);
+  myArr.push(y[i]);
+  }
+  console.log("myArr = ", myArr)
+  let newArray = [];
+  while(myArr.length > 0) {
+    newArray.push(myArr.splice(0,2))
+  }
+  console.log("newArray = ",newArray)
 
-
-
-const y = trips.filter(booked => booked.booked === true)
-console.log("guides = ", guides)
-console.log("y = ", y)
-let myArr = [];
-for (let i = 0; i < guides.length; i ++) {
- myArr.push(guides[i]);
- myArr.push(y[i]);
-}
-console.log("myArr = ", myArr)
-let newArray = [];
-while(myArr.length > 0) {
-  newArray.push(myArr.splice(0,2))
-}
-console.log("newArray = ",newArray)
-if (state.user.user_id) {
-  return ( 
-  
-
-       
+  if(state.user.user_id) {
+    return(
       <div>
-     
-        {/* if the user is logged in, render: */}
-        { trips.length !==0 ?
-        
-          <div>
-            {/* ASK A TA: when I hit refresh, it clears out name? */}
-            {state.user.user_id ? <h1>Welcome back, {state.user.username}</h1> : <h1>You're not logged in!</h1>}
+        <h1>Welcome, {displayName()}</h1>
             <Row>
-            <Col sm={6}>
-              <div>
-                <h2>Booked Adventures</h2>
-                <BookedDeck>
+              <Col sm={6}>
+                <div>
+                  <h2>Booked Adventures</h2>
+                  <BookedDeck>
                     {(newArray.length) ? ( 
-                  
-                   <div>
-                     <div>
-                      {
-                        newArray.map(trip => (trip.length === 2) ? (
-                          <BookedAdventureCard  >
-                         
-                          <h1>{trip[1].username}</h1> 
-                          <p>{trip[1].desc}</p> 
-                         <h2>For more info contact your booked guide: </h2>
-                          <ListGroupItem> <p>Name: {trip[0].firstName} {trip[0].lastName}</p> </ListGroupItem>
-                          <ListGroupItem>  <p>Experience: {trip[0].experience}</p>  </ListGroupItem>
-                          <ListGroupItem> <p>Gender: {trip[0].gender}</p>  </ListGroupItem>
-                          <ListGroupItem> <p>Email: {trip[0].email}</p>  </ListGroupItem>
-                          <ListGroupItem> <p>Phone: {trip[0].cell}</p>  </ListGroupItem>
-                          <ListGroupItem> Date range:  <p>{trip[0].dateRange[0]} to {trip[0].dateRange[1]}</p>  </ListGroupItem>
-                         
-                       
-                         
-                          </BookedAdventureCard>
-                        
-                      
-                        ) : null ) } 
-                        
-                        </div>
-                        <div>
-                       
-                        </div>
-                        </div>
-                    
-                    
+                      <div>
+                          <div>
+                          {newArray.map(trip => (trip.length === 2) ? (
+                            <BookedAdventureCard  >
+                              <h1>{trip[1].username}</h1> 
+                              <p>{trip[1].desc}</p> 
+                              <h2>For more info contact your booked guide: </h2>
+                              <ListGroupItem> <p>Name: {trip[0].firstName} {trip[0].lastName}</p> </ListGroupItem>
+                              <ListGroupItem>  <p>Experience: {trip[0].experience}</p>  </ListGroupItem>
+                              <ListGroupItem> <p>Gender: {trip[0].gender}</p>  </ListGroupItem>
+                              <ListGroupItem> <p>Email: {trip[0].email}</p>  </ListGroupItem>
+                              <ListGroupItem> <p>Phone: {trip[0].cell}</p>  </ListGroupItem>
+                              <ListGroupItem> Date range:  <p>{trip[0].dateRange[0]} to {trip[0].dateRange[1]}</p>  </ListGroupItem>
+                            </BookedAdventureCard>
+                          ) : null ) } 
+                          </div>
+                      <div>
+                      </div>
+                      </div>
                     ) : (
-                    <h3>No trips to display</h3>
+                      <h3>No trips to display</h3>
+                    )}
+                  </BookedDeck>
+                </div>
+              </Col>
+              <Col sm={6}>
+                <div>
+                  <h2>Favorite Adventures</h2>
+                  {trips.length ? ( <BookedDeck> 
+                    {trips.map(trip => ( trip.favorited?
+                      <BookedAdventureCard key={trip._id}>
+                        <Card.Title> <h1>{trip.username} </h1></Card.Title>
+                        <Card.Text>{trip.desc}</Card.Text>
+                        <DeleteBtn onClick={() => deleteTrip(trip._id)} />  
+                      </BookedAdventureCard>
+                    : null ))}
+                  </BookedDeck>
+                  ) : (
+                  <h3>No trips to display</h3>
                   )}
-                </BookedDeck>
-              </div>
-             </Col>
-            
-             <Col sm={6}>
-              <div>
-              
-                <h2>Favorite Adventures</h2>
-                {trips.length ? ( <BookedDeck> 
-                  {trips.map(trip => ( trip.favorited?
-                    <BookedAdventureCard key={trip._id}>
-                      
-                       <Card.Title> <h1>{trip.username} </h1></Card.Title>
-                       <Card.Text>{trip.desc}</Card.Text>
-                    <DeleteBtn onClick={() => deleteTrip(trip._id)} />  
-                  
-                  </BookedAdventureCard>
-                  : null ))}
-                </BookedDeck>
-              ) : (
-              <h3>No trips to display</h3>
-            )}
-      </div>
-      </Col>
-      </Row>
-      </div>
-   
-               
-          : <h4>Login to see where you're going next!</h4>}
-     
-     </div>
-
-  
-   
-  );
-              } else {return (<MustLogin/>)}
+                </div>
+              </Col>
+            </Row> 
+      </div>)
+  } else {
+      return (
+        <MustLogin />
+      )
+  };
 }
   
 export default Trips
