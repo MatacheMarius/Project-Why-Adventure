023155a1aca1c0054633dccd9450510d674c2b7c
Conor O'Brien <obrienceob@gmail.com>
2021-06-03 18:49:36 -0700

    homepage, navbar, footer styling

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -1,20 +1,8 @@
-// import React from 'react';
-// import './Button.css';
-// import { Link } from 'react-router-dom';
-
-// export function Button() {
-//   return (
-//     <Link to='sign-up'>
-//       <button className='btn'>Sign Up</button>
-//     </Link>
-//   );
-// }
-
 import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+const STYLES = ['btn--primary', 'btn--outline'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
@@ -32,9 +20,9 @@ export const Button = ({
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-    <Link to='/sign-up' className='btn-mobile'>
+    <Link to='/PlanYourTrip'>
       <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        className={`${checkButtonStyle} ${checkButtonSize}`}
         onClick={onClick}
         type={type}
       >
