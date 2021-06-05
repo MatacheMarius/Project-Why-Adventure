3eaf4361dc726a819abfe395d45a9edc251eebb5
Conor O'Brien <obrienceob@gmail.com>
2021-06-04 21:51:13 -0700

    .env

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,20 @@
 import axios from "axios";
+require('dotenv').config();
 
  export const getUsers = () => {
     return axios.get("https://randomuser.me/api/?results=1&exc=login");
 };
 
-const getInfo = () => {
+const getInfo = (env) => {
 
   var options = {
     method: 'GET',
     url: 'https://jonahtaylor-national-park-service-v1.p.rapidapi.com/campgrounds',
     params: {stateCode: "WA", 	limit: "9"},
     headers: {
-      'x-api-key': 'CDQqUnod33sYMHzDwqRNsw2lRXMNRdwLZUpiSoJd',
-      'x-rapidapi-key': 'f698202c65msh9632fce62056887p1835f7jsn64c8103bb7d3',
-      'x-rapidapi-host': 'jonahtaylor-national-park-service-v1.p.rapidapi.com'
+      'x-api-key': env.api,
+      'x-rapidapi-key': env.rapidapi,
+      'x-rapidapi-host': env.host
     }
   }
   return axios.request(options)
